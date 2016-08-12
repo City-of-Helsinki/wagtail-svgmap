d3c0bc0a7334fae13f94cfafa37335fc784f41f8
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-08-12 12:44:17 +0300

    Add admin for imagemaps

---
+++ wagtail_svgmap/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.core.urlresolvers import reverse
+
+import pytest
+from wagtail_svgmap.models import ImageMap
+from wagtail_svgmap.tests.utils import IDS_IN_EXAMPLE_SVG
+
+
+@pytest.mark.django_db
+def test_admin(admin_client, example_svg_doc):
+    admin_client.post(reverse('admin:wagtail_svgmap_imagemap_add'), {'svg': example_svg_doc.pk})
+    map = ImageMap.objects.get()
+    edit_data = {
+        '_save': 'Save',
+        'svg': example_svg_doc.pk,
+        'regions-TOTAL_FORMS': 2,
+        'regions-INITIAL_FORMS': 0,
+        'regions-MIN_NUM_FORMS': 0,
+        'regions-MAX_NUM_FORMS': 1000,
+    }
+    for i, data in enumerate([
+        {'link_external': 'http://google.com/foo/', 'element_id': 'blue'},
+        {'link_external': 'http://google.com/bar/', 'element_id': 'red'},
+    ]):
+        data.update({'image_map': map.pk, 'id': ''})
+        for key, value in data.items():
+            edit_data['regions-%d-%s' % (i, key)] = value
+
+    edit_url = reverse('admin:wagtail_svgmap_imagemap_change', args=(map.pk,))
+
+    # Check that the select boxes get populated with the element IDs
+    response = admin_client.get(edit_url, edit_data)
+    response.render()
+    content = response.content.decode('utf-8')
+    for id in IDS_IN_EXAMPLE_SVG:
+        assert id in content
+
+    # Now do some editing!
+    admin_client.post(edit_url, edit_data)
+    map = ImageMap.objects.get()
+    assert map.regions.count() == 2
+    assert '/foo/' in map.rendered_svg
+    assert '/bar/' in map.rendered_svg
